3ddbd6445c809697093683694f46be56a6e035ae
Bob <josua6144@gmail.com>
2026-08-30 11:58:35 +0700

    feat: configure keystatic parallel testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,75 @@
-// @ts-check
+// // @ts-check
+
+// import mdx from '@astrojs/mdx';
+// import sitemap from '@astrojs/sitemap';
+// import { defineConfig, fontProviders } from 'astro/config';
+
+// // https://astro.build/config
+// export default defineConfig({
+// 	site: 'https://nakamuraalamsutera.com',
+// 	integrations: [mdx(), sitemap()],
+// 	fonts: [
+// 		{
+// 			provider: fontProviders.local(),
+// 			name: 'Atkinson',
+// 			cssVariable: '--font-atkinson',
+// 			fallbacks: ['sans-serif'],
+// 			options: {
+// 				variants: [
+// 					{
+// 						src: ['./src/assets/fonts/atkinson-regular.woff'],
+// 						weight: 400,
+// 						style: 'normal',
+// 						display: 'swap',
+// 					},
+// 					{
+// 						src: ['./src/assets/fonts/atkinson-bold.woff'],
+// 						weight: 700,
+// 						style: 'normal',
+// 						display: 'swap',
+// 					},
+// 				],
+// 			},
+// 		},
+// 	],
+// });
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import netlify from '@astrojs/netlify';
+import keystatic from '@keystatic/astro';
 import { defineConfig, fontProviders } from 'astro/config';
 
-// https://astro.build/config
+import react from '@astrojs/react';
+import markdoc from '@astrojs/markdoc';
+
 export default defineConfig({
-	site: 'https://nakamuraalamsutera.com',
-	integrations: [mdx(), sitemap()],
-	fonts: [
-		{
-			provider: fontProviders.local(),
-			name: 'Atkinson',
-			cssVariable: '--font-atkinson',
-			fallbacks: ['sans-serif'],
-			options: {
-				variants: [
-					{
-						src: ['./src/assets/fonts/atkinson-regular.woff'],
-						weight: 400,
-						style: 'normal',
-						display: 'swap',
-					},
-					{
-						src: ['./src/assets/fonts/atkinson-bold.woff'],
-						weight: 700,
-						style: 'normal',
-						display: 'swap',
-					},
-				],
-			},
-		},
-	],
+    site: 'https://nakamuraalamsutera.com',
+    output: 'hybrid',
+    adapter: netlify(),
+    integrations: [mdx(), sitemap(), keystatic(), react(), markdoc()],
+    fonts: [
+        {
+            provider: fontProviders.local(),
+            name: 'Atkinson',
+            cssVariable: '--font-atkinson',
+            fallbacks: ['sans-serif'],
+            options: {
+                variants: [
+                    {
+                        src: ['./src/assets/fonts/atkinson-regular.woff'],
+                        weight: 400,
+                        style: 'normal',
+                        display: 'swap',
+                    },
+                    {
+                        src: ['./src/assets/fonts/atkinson-bold.woff'],
+                        weight: 700,
+                        style: 'normal',
+                        display: 'swap',
+                    },
+                ],
+            },
+        },
+    ],
 });
